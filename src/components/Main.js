7a5bbf7e142b26d74b2ffe3d548ce3cda66a2a9e
wavescats <wavescats@gmail.com>
2022-06-22 09:18:44 +0900

    post

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,35 @@
 import React from "react";
 import "./Main.css";
+import { AiFillEdit } from "react-icons/ai";
 
 function Main() {
   return (
-    <div>
-      <div className="title">글쓰기</div>
+    <div className="main_Container">
+      <div className="main_Wrapper">
+        <div className="main_Profile">
+          <AiFillEdit />
+        </div>
+        <div className="main_Input_Container">
+          <div className="main_Input_Wrapper">
+            <div className="main_Input">
+              <input
+                className="main_Input_Title"
+                type="text"
+                defaultValue=""
+                placeholder="글제목"
+              />
+              <textarea
+                className="main_Input-Message"
+                defaultValue={""}
+                placeholder="글내용"
+              />
+            </div>
+            <div className="main_Posting">
+              <button className="main_Posting_Btn">글쓰기</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
